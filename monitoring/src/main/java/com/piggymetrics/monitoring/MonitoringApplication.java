29787c2b5e9b819f357b4467807f0a823ea342b0
Qingfei Yuan <yuanqingfei@gmail.com>
2022-01-16 04:47:22 -0500

    update monitoring and turbine

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -2,10 +2,8 @@ package com.piggymetrics.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrixDashboard
 public class MonitoringApplication {
 
 	public static void main(String[] args) {
